0a0ebf09f0c62e62512f08d07bf921ba18431f47
Kauan Constancio <146592614+kauanconstancio@users.noreply.github.com>
2024-11-30 21:40:04 +0000

    Desafio concluidos!

---
+++ app.js
@@ -0,0 +1,31 @@
+// Desafio 1 
+// Altere o conteúdo da tag h1 com document.querySelector e atribua o seguinte texto: Hora do Desafio.
+// (Concluido)
+
+// let titulo  = document.querySelector('h1');
+// titulo.innerHTML = 'Hora do Desafio!';
+
+// Desafio 2 
+// Crie uma função que exiba no console a mensagem O botão foi clicado sempre que o botão Console for pressionado.
+// (Concluido)
+
+// function avisarConsole() {
+//     console.log('O botão foi clicado!')
+// };
+
+// Desafio 3 
+// Crie uma função que exiba um alerta com a mensagem: Eu amo JS, sempre que o botão Alerta for pressionado.
+// (Concluido)
+
+// function alertaJs() {
+//     alert('eu amo JS!')
+// };
+
+// Desafio 4 
+// Crie uma função que é executada quando o botão prompt é clicado, perguntando o nome de uma cidade do Brasil. Em seguida, exiba um alerta com a mensagem concatenando a resposta com o texto: Estive em {cidade} e lembrei de você.
+//(concluido)
+
+// function nomeCidade() {
+//     let nomeDaCidade = prompt('Digite o nome de uma cidade do Brasil!')
+//     alert(`Estive em ${nomeDaCidade} e lembrei de você!`)
+// };
